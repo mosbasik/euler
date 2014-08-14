9ade00a1d30e64012db6254af78db028f2265876
Peter Henry <phenryextra@zoho.com>
2014-08-13 22:09:43 -0500

    003 working for small numbers

---
+++ 003/003.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# https://projecteuler.net/problem=3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+import sys
+
+def isPrime(N):
+    if N <= 1: return 0
+    i = 2
+    while (i * i <= N):
+        if (N % i == 0):
+            return 0
+        i +=1
+    return 1
+
+
+def greatestPrimeFactor(N):
+    for i in range(N, 1, -1):
+        if (N % i == 0):
+            if isPrime(i):
+                return i
+    assert(False)
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    print "is %d prime? %d" % (n, isPrime(n))
+    print "%d's greatest prime factor: %d" % (n, greatestPrimeFactor(n))
