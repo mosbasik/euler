a3256d803ebbbd8900de7b61d33373522cdfaf84
Peter Henry <phenryextra@zoho.com>
2014-08-13 23:49:46 -0500

    003 complete

--- 003/003.py
+++ 003/003.py
@@ -4,7 +4,10 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+
 import sys
+import math
+
 
 def isPrime(N):
     if N <= 1: return 0
@@ -17,7 +20,9 @@ def isPrime(N):
 
 
 def greatestPrimeFactor(N):
-    for i in range(N, 1, -1):
+    rootN = int(math.ceil(math.sqrt(N)))
+    for i in range(rootN, 1, -1):
+        print i
         if (N % i == 0):
             if isPrime(i):
                 return i
