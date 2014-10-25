c503a42ea1420b729c2c6420e16962f07d32b01b
Peter Henry <phenryextra@zoho.com>
2014-10-25 18:30:22 -0500

    working iterative implementation of get_pythagorean_triplet()

--- 009/009.py
+++ 009/009.py
@@ -3,7 +3,7 @@
 # # https://projecteuler.net/problem=9
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 # a^2 + b^2 = c^2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
+# For example, 3^2 + 4^2 = 5^2.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
@@ -14,28 +14,19 @@ import sys
 import math
 
 
-def get_pythagorean_triplet(a=1, b=2):
+def get_pythagorean_triplet():
+    a = 1
+    b = 2
     while True:
         c = math.sqrt(a**2 + b**2)
-        # print str(a) + ' ' + str(b) + ' ' + str(c)
-
         if c % 1 == 0:
             yield (a, b, int(c))
-
-            if a+1 == b:
-                a = 1
-                b += 1
-
-            else:
-                a += 1
         
-        elif a+1 == b:
-            for trip in get_pythagorean_triplet(1, b+1):
-                yield trip
-
+        if a + 1 != b:
+            a += 1
         else:
-            for trip in get_pythagorean_triplet(a+1, b):
-                yield trip
+            a = 1
+            b += 1
 
 
 if __name__ == '__main__':
